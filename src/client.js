feabeb244f23688a52af222961a4bd3cbbceed77
Francesco REALI <francesco.reali@alten.it>
2020-05-27 21:25:57 +0200

    whoox3

--- src/client.js
+++ src/client.js
@@ -1,23 +1,6 @@
 import * as sapper from '@sapper/app';
-import { createProxyClient } from 'ipfs-postmsg-proxy';
-
-const swName = 'ipfs-worker.js'
-let node
 
 sapper.start({
 	target: document.querySelector('#sapper')
 });
 
-
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register(swName)
-	.then((registration) => {
-		node = createProxyClient({
-			addListener: navigator.serviceWorker.addEventListener.bind(navigator.serviceWorker),
-			postMessage: (data) => navigator.serviceWorker.controller.postMessage(data)
-		})
-	})
-	.catch((err) => {
-		console.log('-> Failed to register:', err)
-	})
-}
